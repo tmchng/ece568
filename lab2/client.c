977ef3819a9c9b0b997632dfda5f0b41832eb976
Tim Cheng <timcheng1006@gmail.com>
2015-02-28 00:12:00 -0500

    reverted yuan's edit to common

--- lab2/client.c
+++ lab2/client.c
@@ -13,6 +13,8 @@
 
 #define HOST "localhost"
 #define PORT 8765
+#define CLIENT_KEYFILE "alice.pem"
+#define CLIENT_KEYFILE_PWD "password"
 #define SERVER_CERT_CN "Bob's Server"
 #define SERVER_CERT_EMAIL "ece568bob@ecf.utoronto.ca"
 
@@ -60,17 +62,45 @@ int tcp_connect(char *host, int port) {
   return sock;
 }
 
-void ssl_init() {
-  //OpenSSL_add_all_algorithms();
-  SSL_library_init();
-  SSL_load_error_strings();
-  ERR_load_BIO_strings();
-  //ERR_load_crypto_strings();
-}
 
+SSL_CTX *init_ctx(char *keyfile, char *password, int ctx_opts) {
+  const SSL_METHOD *method;
+  SSL_CTX *ctx;
 
-void ssl_connect() {
-  ssl_init();
+  method = SSLv23_method();
+  ctx = SSL_CTX_new(method);
+
+  // Option flags can be used to disable SSLv2
+  SSL_CTX_set_options(ctx, ctx_opts);
+
+  // Load keys and certificates
+  if (!SSL_CTX_use_certificate_chain_file(ctx, keyfile)) {
+    printf(FMT_CONNECT_ERR);
+    ERR_print_errors(bio_err);
+    printf("Cannot read certificate file\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // Set password used to decrypt private key
+  SSL_CTX_set_default_passwd_cb_userdata(ctx, password);
+
+  // Read private key
+  if (!SSL_CTX_use_PrivateKey_file(ctx, keyfile, SSL_FILETYPE_PEM)) {
+    printf(FMT_CONNECT_ERR);
+    ERR_print_errors(bio_err);
+    printf("Cannot read key file\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // Load trusted CA
+  if (!SSL_CTX_load_verify_locations(ctx, TRUSTED_CA, 0)) {
+    printf(FMT_CONNECT_ERR);
+    ERR_print_errors(bio_err);
+    printf("Cannot read CA\n");
+    exit(EXIT_FAILURE);
+  }
+
+  return ctx;
 }
 
 
@@ -84,25 +114,19 @@ void check_cert(SSL *ssl, char *correct_CN, char *correct_email) {
 
   if (SSL_get_verify_result(ssl) != X509_V_OK) {
     printf(FMT_NO_VERIFY);
+    ERR_print_errors(bio_err);
     exit(EXIT_FAILURE);
   }
 
-  peer = SSL_get_peer_certificate(ssl);
-  peer_subject_name = X509_get_subject_name(peer);
-
-  // Get Common Name
-  X509_NAME_get_text_by_NID(peer_subject_name, NID_commonName, peer_CN, STR_LEN);
-  // Get email address
-  X509_NAME_get_text_by_NID(peer_subject_name, OBJ_txt2nid("emailAddress"), peer_email, STR_LEN);
-
-  // Check CN
   if (strcasecmp(peer_CN, correct_CN)) {
     printf(FMT_CN_MISMATCH);
+    ERR_print_errors(bio_err);
     error = 1;
   }
   // Check email
   if (strcasecmp(peer_email, correct_email)) {
     printf(FMT_EMAIL_MISMATCH);
+    ERR_print_errors(bio_err);
     error = 1;
   }
 
@@ -110,6 +134,56 @@ void check_cert(SSL *ssl, char *correct_CN, char *correct_email) {
     exit(EXIT_FAILURE);
   }
 }
+
+
+void ssl_init() {
+  if (!bio_err) {
+    // Init things necessary to make SSL work
+    //OpenSSL_add_all_algorithms();
+    SSL_library_init();
+    SSL_load_error_strings();
+    ERR_load_BIO_strings();
+    //ERR_load_crypto_strings();
+    bio_err = BIO_new_fp(stdout, BIO_NOCLOSE);
+  }
+}
+
+
+void ssl_connect(char* host, int port, char *keyfile, char *password) {
+  SSL_CTX *ctx;
+  X509 *cert = NULL;
+  X509_NAME *cert_name = NULL;
+  SSL *ssl;
+  int tcp_sock;
+
+  // Create tcp connection
+  tcp_sock = tcp_connect(host, port);
+
+  if (tcp_sock == 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  // Init things necessary to run ssl
+  ssl_init();
+
+  ctx = init_ctx(CLIENT_KEYFILE, CLIENT_KEYFILE_PWD, SSL_OP_NO_SSLv2);
+  ssl = SSL_new(ctx);
+
+  if (!SSL_set_fd(ssl, tcp_sock)) {
+    printf(FMT_CONNECT_ERR);
+    printf("Cannot join ssl and tcp handle\n");
+  }
+
+  // Initiate SSL handshake
+  if (SSL_connect(ssl) <= 0) {
+    printf(FMT_CONNECT_ERR);
+    printf("SSL connect error\n");
+  }
+
+  // Check certificate
+  check_cert(ssl, SERVER_CERT_CN, SERVER_CERT_EMAIL);
+}
+
 
 int main(int argc, char **argv)
 {
--- lab2/ssl_common.h
+++ lab2/ssl_common.h
@@ -5,38 +5,14 @@
 #ifndef _ssl_common_h
 #define _ssl_common_h
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netinet/tcp.h>
-#include <netdb.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <unistd.h>
-#include <string.h>
-
 #include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <openssl/pem.h>
+#include <openssl/evp.h>
 
-#define CA_LIST "568ca.pem"
-#define HOST	"localhost"
-#define RANDOM  "random.pem"
-#define PORT	8765
-#define BUFSIZZ 256
+#define TRUSTED_CA "568ca.pem"
 
+// The BIO object for printing error.
 extern BIO *bio_err;
-int berr_exit (char *string);
-int err_exit(char *string);
-
-SSL_CTX *initialize_ctx(char *keyfile, char *password);
-void destroy_ctx(SSL_CTX *ctx);
-
-#ifndef ALLOW_OLD_VERSIONS
-#if (OPENSSL_VERSION_NUMBER < 0x00905100L)
-#error "Must use OpenSSL 0.9.6 or later"
-#endif
-#endif
 
 #endif
